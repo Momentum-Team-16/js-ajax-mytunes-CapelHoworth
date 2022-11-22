1940008e3d7d7cda0c27fcd2c238a9833d538e3a
CapelHoworth <capelhoworth@gmail.com>
2022-11-22 16:12:23 -0500

    got bulma to flex-wrap

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let searchTerm;
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  //   tag on the search input
+  //   put in the search input
   searchTerm = url + form.querySelector("#key").value + "/";
   console.log("submitted");
   fetch(searchTerm, {
@@ -18,6 +18,20 @@ form.addEventListener("submit", (event) => {
     .then((data) => {
       for (let song of data.results) {
         console.log(`This is what we got from the API: ${song.trackName}`);
+        let card = document.createElement("div");
+        card.classList.add("card", "tile", "is-3");
+        // card.classList.add("tile");
+        let image = document.createElement("img");
+        let figure = document.createElement("figure");
+        figure.classList.add("image", "is-4by3");
+        figure.appendChild(image);
+        image.src = song.artworkUrl100;
+        card.appendChild(image);
+        let title = document.createElement("div");
+        title.classList.add("title", "is-4");
+        title.innerText = song.trackName;
+        card.appendChild(title);
+        results.appendChild(card);
       }
     });
 });
